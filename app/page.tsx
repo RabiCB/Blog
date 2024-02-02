83b1098ec9bc558a967e4fc675fe9fb08f1a3d94
Chandra Bhandari <rollinrabin@gmail.com>
2024-02-03 01:19:35 +0545

    feat:meta verification tags

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,17 @@ import { Client, urlFor } from "./lib/sanity";
 import { blogDatatype } from "./lib/interface";
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
+import { Metadata } from "next";
+
+
+export const metadata:Metadata={
+  title:"Blogcb",
+  description:"Blogs and Latest technology articles",
+  verification:{
+google:"rqoGcoydFL1P8M80747RFMH41LZkBG1Ti4eCLGPiAEo"
+  }
+
+}
 
 async function getData() {
   const data = await Client.fetch(getBlogList);
@@ -13,7 +24,7 @@ async function getData() {
 
 export default async function Home() {
   const data: blogDatatype[] = await getData();
-  console.log(data);
+
   return (
     <div className="grid grid-cols-2 gap-5  max-sm:grid-cols-1 mt-8 ">
       {data?.map((post) => {
