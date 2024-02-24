1de01bc33b859d1a7e3a6ab20601ea9acd5ea7f9
Chandra Bhandari <rollinrabin@gmail.com>
2024-02-24 16:30:19 +0545

    feat:comes padding

--- components/card/Blogcard.tsx
+++ components/card/Blogcard.tsx
@@ -31,7 +31,7 @@ const Blogcard = ({
           src={urlFor(titleImage).url()}
         />
       </div>
-      <div className="mt-1 ml-4 max-md:mt-2 max-md:ml-0   w-[60%] max-md:w-full">
+      <div className="mt-2 ml-4 max-md:mt-2 max-md:ml-0 text-justify   w-[60%] max-md:w-full">
         <p className="text-xs  max-md:pt-0 dark:text-white max-sm:text-center   text-gray-600">
           {smalldescription}
         </p>
